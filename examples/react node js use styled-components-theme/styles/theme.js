b41fd12c1a21d1388e737b3eb847c48610c9f009
Epikem <dltldls95@naver.com>
2018-04-18 16:42:39 +0900

    add example react node js use styled-components-theme

---
+++ examples/react node js use styled-components-theme/styles/theme.js
@@ -0,0 +1,7 @@
+// theme.js
+
+import createTheme from 'styled-components-theme';
+import colors from './colors' // from Step #1
+
+const theme = createTheme(...Object.keys(colors))
+export default theme
