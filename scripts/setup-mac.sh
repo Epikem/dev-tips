8c0324730178386d3043de4227d6a073f4f93a4d
epikem <epikem@epikems-MacBook-Pro.local>
2020-07-25 14:05:36 +0900

    add setup-mac.sh

---
+++ scripts/setup-mac.sh
@@ -0,0 +1,10 @@
+brew cask install rectangle
+brew cask install firefox
+brew cask install app-cleaner
+brew cask install zoomus
+brew cask install visual-studio-code
+brew cask install dropbox
+brew cask install java
+brew cask install anaconda
+
+brew install graphviz
