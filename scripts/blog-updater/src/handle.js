aa6edb26e8cc45e435ab993c49ece10188d2feff
epikem <epikem@kakao.com>
2021-07-19 14:46:28 +0900

    update handler

--- scripts/blog-updater/src/handle.js
+++ scripts/blog-updater/src/handle.js
@@ -12,11 +12,12 @@ import _ from 'lodash';
 import tagHandler from './tag-handler.js';
 
 // WORKING DIRECTORY: ./scripts/blog-updater
-const YEAR = '2021';
+const YEAR = process.env.YEAR || '2021';
 // const IN_DIR = './src/' + YEAR;
-const IN_DIR = path.join(process.env.SOURCE_DIR) || './src/' + YEAR;
+const IN_DIR = path.join(process.env.SOURCE_DIR || './src/' + YEAR);
 // const OUT_DIR = './src/test-out-dir';
-const OUT_DIR = path.join(process.env.TARGET_DIR) || './src/test-out-dir';
+const OUT_DIR = path.join(process.env.TARGET_DIR || './src/test-out-dir');
+console.log('target year', YEAR);
 console.log('source dir', IN_DIR);
 console.log('target dir', OUT_DIR);
 
