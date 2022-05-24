14071820d7dd9c748ed89fed321b8caae038fc21
Hyunwoo Lee <epikem@kakao.com>
2022-05-24 20:40:04 +0900

    add 2022-05-24 - add leet solution

---
+++ sport/solutions/leet/search-in-rotated-sorted-array.py
@@ -0,0 +1,259 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/python
+
+# #NYAN NYAN
+#░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+#░░░░░░░░░░▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄░░░░░░░░░
+#░░░░░░░░▄▀░░░░░░░░░░░░▄░░░░░░░▀▄░░░░░░░
+#░░░░░░░░█░░▄░░░░▄░░░░░░░░░░░░░░█░░░░░░░
+#░░░░░░░░█░░░░░░░░░░░░▄█▄▄░░▄░░░█░▄▄▄░░░
+#░▄▄▄▄▄░░█░░░░░░▀░░░░▀█░░▀▄░░░░░█▀▀░██░░
+#░██▄▀██▄█░░░▄░░░░░░░██░░░░▀▀▀▀▀░░░░██░░
+#░░▀██▄▀██░░░░░░░░▀░██▀░░░░░░░░░░░░░▀██░
+#░░░░▀████░▀░░░░▄░░░██░░░▄█░░░░▄░▄█░░██░
+#░░░░░░░▀█░░░░▄░░░░░██░░░░▄░░░▄░░▄░░░██░
+#░░░░░░░▄█▄░░░░░░░░░░░▀▄░░▀▀▀▀▀▀▀▀░░▄▀░░
+#░░░░░░█▀▀█████████▀▀▀▀████████████▀░░░░
+#░░░░░░████▀░░███▀░░░░░░▀███░░▀██▀░░░░░░
+#░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+
+false = False
+true = True
+null = None
+# import math
+TEST = false
+try:
+    import sys
+    for arg in sys.argv:
+        if(arg == 'test'):
+            print('test mode')
+            TEST = True
+            print(sys.version)
+    pass
+except:
+    pass
+
+
+def AddImports(libraryNames):
+    for libname in libraryNames:
+        if (type(libname) == type(tuple())):
+            short = libname[1]
+            libname = libname[0]
+        else:
+            short = None
+        try:
+            lib = __import__(libname)
+        except ImportError:
+            pass
+        else:
+            if short:
+                globals()[short] = lib
+            else:
+                globals()[libname] = lib
+    return True
+
+
+# libnames = ['fileinput', 'codecs', 'operator', 'functools', 'math',
+#             'io', 'platform', 'collections', 'mmap', 'logging', 'logging.handlers']
+libnames = ['functools', 'math', 'collections']
+# libnames = ['math']
+
+AddImports(libnames)
+IntellisenseHint = False
+if IntellisenseHint:
+    import functools
+    import math
+    import collections
+    # import mmap
+    # import logging
+    # import logging.handlers
+    # import defs
+
+from typing import List
+
+class memoized(object, ):
+    "Decorator. Caches a function's return value each time it is called.\n\tIf called later with the same arguments, the cached value is returned\n\t(not reevaluated).\n\t"
+
+    def __init__(self, func):
+        self.func = func
+        self.cache = {}
+
+    def __call__(self, *args):
+        if (not isinstance(args, collections.Hashable)):
+            return self.func(*args)
+        if (args in self.cache):
+            return self.cache[args]
+        else:
+            value = self.func(*args)
+            self.cache[args] = value
+            return value
+
+        def __repr__(self):
+            "Return the function's docstring."
+            return self.func.__doc__
+
+        def __get__(self, obj, objtype):
+            'Support instance methods.'
+            return functools.partial(self.__call__, obj)
+
+
+def it(args, *arg):
+    if(TEST):
+        print(args, *arg)
+    # print(args, vargs)
+
+    pass
+
+
+def floatEqual(a, b):
+    diff = math.fabs(a-b)
+    if(diff < 1e-10):
+        return True
+    else:
+        return diff <= 1e-8 * max(math.fabs(a), math.fabs(b))
+
+def ria():
+    return list(map(int, input().strip().strip(' ').split(' ')))
+
+def rsa():
+    return list(map(str, input().strip().strip('\n').split(' ')))
+
+# def create2dArray(y,x,val=False):
+#     arr = [val]*x
+#     for xx in range(x):
+#         if(y == 0):
+#             arr[xx] = []
+#         else: arr[xx] = [val]*y
+#     return arr
+
+def create2DArray(rows,cols,val=False):
+    return [[val for c in range(cols)] for r in range(rows)]
+
+def create2DNumpyZeroArray(rows, cols, dtype):
+    import numpy as np
+
+    return np.zeros((rows,cols), dtype=dtype)
+
+def create3DArray(zs,ys,xs,val=False):
+    return [[[val for x in range(xs)] for y in range(ys)] for z in range(zs)]
+
+def print2DArray(array):
+    for r in range(len(array)):
+        it(*array[r])
+
+def takeFirst(x):
+    return x[0]
+
+def takeSecond(x):
+    return x[1]
+
+from sys import stdin
+input=stdin.readline
+import copy
+# import queue
+from collections import deque
+
+# from itertools import permutations
+sys.setrecursionlimit(10000)
+
+def join(targetType, sourceList):
+    return ''.join(map(targetType, sourceList))
+
+def getnext(n, k):
+    if(n==k):
+        return 1
+    else:
+        return k+1
+
+def genNums(n):
+    import random
+    arr = []
+    for i in range(n):
+        arr.append(random.randint
+        (0, 100000))
+    return arr
+
+# ordCache={}
+# def getD(c):
+#     global ordCache
+#     if(c in ordCache):
+#         return ordCache[c]
+#     ordCache[c] = ord(c) - 96
+#     return ordCache[c]
+
+@memoized
+def getD(c):
+    return ord(c) - 96
+
+@memoized
+def getCh(o):
+    while(o<ord('a')):
+        o+=26
+    while(o>ord('z')):
+        o-=26
+    return chr(o)
+        
+def search(nums: List[int], target: int) -> int:
+    import math
+    first = nums[0]
+    last = nums[-1]
+    cnt = len(nums)
+
+    if target >= nums[0]:
+        le = 0
+        ri = len(nums)
+        prev = -99999
+        while (le <= ri):
+            mid = math.floor((le + ri) / 2)
+            if (mid >= cnt):
+                return -1
+            cur = nums[mid]
+            print('1le',le,'ri',ri,'mid',mid, 'cur',cur)
+            if (cur == target):
+                return mid
+            if (cur < first):
+                ri = mid
+            elif (cur > target):
+                ri = mid
+            elif (cur < target):
+                le = mid + 1
+            if (mid == prev):
+                return -1
+            prev = mid
+        return le 
+
+    le = -1
+    ri = len(nums) - 1
+    prev = -99999
+    while (le <= ri):
+        mid = math.ceil((le + ri) / 2)
+        cur = nums[mid]
+        print('2le',le,'ri',ri,'mid',mid, 'cur',cur)
+        if (cur == target):
+            return mid
+        if (cur > last):
+            le = mid
+        elif (cur > target):
+            ri = mid - 1
+        elif (cur < target):
+            le = mid
+        if (mid == prev):
+            return -1
+        prev = mid
+    return le
+
+def solve():
+#     [4,5,6,7,0,1,2]
+# 6
+    # res = search([1,3], 0)
+    # res = search([8,10,12,-2,0,3,5], 1)
+    res = search([1], 2)
+    print(res)
+    pass
+
+    
+
+solve()
+# import profile
+# profile.run("solve()")
+
