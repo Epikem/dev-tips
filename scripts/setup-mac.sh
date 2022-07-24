4393726211a9dcb24731018f121e0f4f6283d2de
hyunwoo.lee <hyunwoo.lee@streami.co>
2022-07-24 15:28:18 +0900

    add 2022-07-24 - update setup-mac scripts

--- scripts/setup-mac.sh
+++ scripts/setup-mac.sh
@@ -11,3 +11,16 @@ brew install graphviz
 brew install asciinema
 brew install gpg
 
+brew install wget
+
+# install oh-my-zsh
+sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+
+brew install hyper
+brew install macs-fan-control
+brew install scroll-reverser
+brew install gpg2
+brew install git-secret
+brew install shellcheck
+
+
